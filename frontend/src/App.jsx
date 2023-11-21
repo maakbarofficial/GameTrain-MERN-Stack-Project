62990b66c8bb4eccd5e096be843329f0790680da
MAliAKbar22 <maakbarofficial@gmail.com>
2023-11-21 20:19:13 +0500

    Frontend Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,29 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
+import axios from "axios";
+import { Context, server } from "./main";
 
 function App() {
+  const { setUser, setIsAuthenticated, setLoading } = useContext(Context);
+
+  useEffect(() => {
+    // setLoading(true);
+    axios
+      .get(`${server}/user/:id`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setUser(res.data.user);
+        setIsAuthenticated(true);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setUser({});
+        setIsAuthenticated(false);
+        setLoading(false);
+      });
+  }, []);
   return (
     <Router>
       <Header />
